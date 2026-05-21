c5cd9338bc85bcce90b6e4b3e9135926afb1a76c
lydia205 <lydiaomombo35@gmail.com>
2026-05-21 00:51:41 -0700

    added the UI in my code

--- app/src/main/java/com/example/myltiplyapp/MainActivity.kt
+++ app/src/main/java/com/example/myltiplyapp/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.example.myltiplyapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    // get id from the UI
+    var num1 = findViewById<EditText>(R.id.editNumF)
+    var num2 = findViewById<EditText>(R.id.editNumS)
+    var btnCalc = findViewById<Button>(R.id.btnCalc)
+    var txtAns =  findViewById<TextView>(R.id.txtAns)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
